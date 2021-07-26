17639c30c05364695b834d63ddd7a21f6a223438
HanNPC <raihanalwan2132@gmail.com>
2021-07-26 17:13:24 +0700

    tugas 1

---
+++ soal1.js
@@ -0,0 +1,9 @@
+function nm(p, l) {
+    luas = p * l
+    document.write("Panjang : " + p + "<br>")
+    document.write("Lebar : " + l + "<br>")
+    document.write("luas persegi panjang : " + luas)
+}
+inputp = prompt("Masukan Nilai Panjang")
+inputl = prompt("Masukan Nilai Lebar")
+nm(inputp, inputl)
